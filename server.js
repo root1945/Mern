4d007142a94c1d982c9b3a57d0cd188e10f289d5
Victor Alencar <munternl13@gmail.com>
2021-08-25 02:06:04 -0300

    connection with mongodb and a previous mongoose version

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 5000
+
+mongoose.connect('mongodb://localhost:27017/mern', {
+  useUnifedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+}, (err) => {
+  if(err){
+    console.log(err)
+  }else {
+    console.log('MongoDB Conectado com sucesso')
+  }
+})
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json())

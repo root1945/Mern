f3e30326ccdbcb6c18c1655eb5ab3dfb1bed273b
Victor Alencar <munternl13@gmail.com>
2021-08-25 01:31:36 -0300

    Added nodemon and file server.js

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 5000
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+
+app.get('/home', (req, res) => {
+  res.json({message: "Hello Word"})
+})
+
+app.listen(3000, () => {
+  console.log(`Server runing on port ${3000}`)
+})

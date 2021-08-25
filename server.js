e7c187755707c5853e731141cb4c0c435faa5c46
Victor Alencar <munternl13@gmail.com>
2021-08-25 01:48:32 -0300

    Added mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 5000

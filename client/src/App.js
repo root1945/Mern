949e9bb7707ba3b33d8fb862f5668fcd6a415cbc
Victor Alencar <munternl13@gmail.com>
2021-08-27 22:22:47 -0300

    Added front-end Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import Inicio from '../src/pages/client/painel'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Projeto Mern</h1>
-      </header>
+   {/*<header className="App-header">
+      <h1>Projeto Mern</h1>
+      </header>*/}
+      <Inicio/>
     </div>
   );
 }

5d381a42de612911e69bc86e7325e43a026c14e4
Victor Alencar <munternl13@gmail.com>
2021-09-11 03:02:27 -0300

    Save User Registration with NodeJS and ReactJS

--- client/src/pages/admin/users/usuario.cadastrar.js
+++ client/src/pages/admin/users/usuario.cadastrar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
@@ -12,6 +12,8 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import MenuAdmin from '../../../components/menu.admin';
 import Copyright from '../../../components/footer.admin'
+import Button from '@material-ui/core/Button';
+import api from '../../../services/api'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,10 +45,26 @@ const useStyles = makeStyles((theme) => ({
 
 export default function UsuarioCadastrar() {
   const classes = useStyles();
-  const [tipo, setTipo] = React.useState('');
-  const handleChange = (event) => {
-    setTipo(event.target.value);
-  };
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [tipo, setTipo] = useState('');
+
+  async function handleSubmit(){
+    const data = {
+      name_usuario: nome, 
+      email_usuario: email, 
+      senha_usuario: senha, 
+      tipo_usuario: tipo
+    }
+    const response = await api.post('/api/usuarios', data)
+    if(response.status === 200){
+      window.location.href='/admin/usuarios'
+    }else {
+      alert('Error ao cadastrar!')
+    }
+  }
+
   return (
     <div className={classes.root}>
       <MenuAdmin title={'Usuários'} />
@@ -64,6 +82,8 @@ export default function UsuarioCadastrar() {
             label="Nome Completo"
             fullWidth
             autoComplete="nome"
+            value={nome}
+            onChange={e => setNome(e.target.value)}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -74,6 +94,8 @@ export default function UsuarioCadastrar() {
             label="Email"
             fullWidth
             autoComplete="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -83,7 +105,7 @@ export default function UsuarioCadastrar() {
             labelId="labelTipo"
             id="tipo"
             value={tipo}
-            onChange={handleChange}
+            onChange={e => setTipo(e.target.value)}
           >
           <MenuItem value={1}>Administrador</MenuItem>
           <MenuItem value={2}>Funcionário</MenuItem>
@@ -99,7 +121,14 @@ export default function UsuarioCadastrar() {
             label="Senha"
             fullWidth
             autoComplete="senha"
+            value={senha}
+            onChange={e => setSenha(e.target.value)}
           />
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <Button variant="contained" onClick={handleSubmit} color="primary">
+            Salvar
+          </Button>
         </Grid>
         </Grid>
         </ Paper>
